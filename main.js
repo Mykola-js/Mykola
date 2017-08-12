7b87bf17a582caaf5582a16c575d7c294eb932ac
sambis <nukolia@mail.ru>
2017-08-12 22:03:28 +0300

    The work is complete

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let fs= require('fs');
 let AdmZip = require('adm-zip');
 let csv_sync_parse = require('csv-parse/lib/sync');
+let jsonfile = require('jsonfile');
+
 
 let zip = new AdmZip("./model/archives/data.zip");
     let zipEntries = zip.getEntries(); 
@@ -32,3 +34,28 @@ users.map( function(item){
 
 // concating all arrays in one
 let users_All= [].concat.apply([], users)
+
+
+//location users.json
+let JSON_file = './model/users.json'
+fs.unlink( JSON_file )
+
+// Interface formation
+users_All.map( function(user){
+	
+	//Date formatting
+	let date_format = user[8].split('/')
+			
+	let user_data= {
+		"name": `${user[0]} ${user[1]}`,
+		"phone": user[5].replace(/[() -]/g, ''),
+		"person": {
+			"firstName": user[0],
+			"lastName": user[1],
+		},
+		"amount": user[7],
+		"date":  date_format[2]+'-'+date_format[1]+'-'+date_format[0],
+		"costCenterNum": user[6].match(/[0-9]{1,}/)[0] 
+	}
+	jsonfile.writeFileSync(JSON_file, user_data, {flag: 'a'})
+} )
