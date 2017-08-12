b1c96e6a2401562c54be1fd4c3f48c87b635a218
sambis <nukolia@mail.ru>
2017-08-12 11:05:32 +0300

    Extracts files from zip

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 let fs= require('fs');
 let AdmZip = require('adm-zip');
 
-var zip = new AdmZip("./model/archives/data.zip");
-    var zipEntries = zip.getEntries(); 
+let zip = new AdmZip("./model/archives/data.zip");
+    let zipEntries = zip.getEntries(); 
  
-    
+	//Extracts files
+    let users_files_names = zipEntries.map( function(item){
+	let itemName = item.name
+	if( itemName.search(/^users[0-9]{1,5}.csv$/) === 0 ){
+		return itemName
+	}
+} )
+
+
